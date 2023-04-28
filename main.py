f096780111686cc04f11c3363fa39803e6b30848
hujieisme <74301537+hujieisme@users.noreply.github.com>
2023-04-28 12:56:28 +0800

    大版本更新，引入通道选择，MCU启动控制等等

--- main.py
+++ main.py
@@ -24,12 +24,9 @@ sys_platform = platform.platform().lower()
 global UART
 global RX_THREAD
 global gui
-global app, data, \
-    curve1, p1, curve2, p2, \
-    curve3, p3, curve4, p4, \
-    curve5, p5, curve6, p6, \
-    curve7, p7, curve8, p8, \
-    curve9, p9, curve10, p10
+global app, data, p_list, curve_list
+p_list = []
+curve_list = []
 
 class Plot_Widget(pg.PlotWidget):
 
@@ -48,12 +45,17 @@ class MainWindow(QMainWindow):
 
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.VL = None
+        self.HL = None
         self.pages_manager = None
         self.plot_page = None
         self.setting_page = None
         self.coms = None
         self.flag = bool(0)
         self.flag_2 = bool(0)
+        self.flag_3 = bool(0)
+        self.channals_num = 2
+        self.str2send = ''
         self.callBack = []
         app = pg.mkQApp("Plotting Example")
         self.init_UI()
@@ -68,7 +70,7 @@ class MainWindow(QMainWindow):
 
         # uic.loadUi(Plot_Page, self.plot_page)
 
-        self.resize(250, 280)
+        self.resize(600, 400)
         self.setWindowTitle("数据流串口终端")
         self.setWindowIcon(QIcon('graphs.ico'))
         self.action.triggered.connect(self.setting_page_show)
@@ -76,6 +78,9 @@ class MainWindow(QMainWindow):
         self.setting_page.pushButton.clicked.connect(self.update_Coms)
         self.setting_page.pushButton_2.clicked.connect(self.open_close_coms)
         self.setting_page.pushButton_3.clicked.connect(self.write_bound_rate)
+        self.action_3.triggered.connect(self.start_MCU)
+        self.setting_page.pushButton_7.clicked.connect(self.sendcommand)
+        self.setting_page.pushButton_8.clicked.connect(self.set_channals)
 
         self.pages_manager = QStackedWidget(self)
         self.pages_manager.addWidget(self.setting_page)
@@ -85,27 +90,27 @@ class MainWindow(QMainWindow):
 
     def init_Plot(self):
         global data
-        global curve1, p1, curve2, p2, \
-            curve3, p3, curve4, p4, \
-            curve5, p5, curve6, p6, \
-            curve7, p7, curve8, p8, \
-            curve9, p9, curve10, p10
+        global p_list, curve_list
 
         self.plot_page = QWidget()
         data = np.zeros((10, 32000))
-        VL = QVBoxLayout()
-        HL = QHBoxLayout()
-        self.plot_page.setLayout(VL)
-        p1 = Plot_Widget()
-        p1.setLabel('top', 'CH1')
-        curve1 = p1.plot(pen='y')
-        p2 = Plot_Widget()
-        p2.setLabel('top', 'CH2')
-        curve2 = p2.plot(pen='g')
-        VL.addWidget(p1)
-        VL.addWidget(p2)
-        curve1.setData(data[0])
-        curve2.setData(data[1])
+        self.VL = QVBoxLayout()
+        self.HL = QHBoxLayout()
+        self.plot_page.setLayout(self.VL)
+        p = Plot_Widget()
+        p.setLabel('top', 'CH1')
+        curve = p.plot(pen='y')
+        p_list.append(p)
+        curve_list.append(curve)
+        p = Plot_Widget()
+        p.setLabel('top', 'CH2')
+        curve = p.plot(pen='g')
+        p_list.append(p)
+        curve_list.append(curve)
+        self.VL.addWidget(p_list[0])
+        self.VL.addWidget(p_list[1])
+        curve_list[0].setData(data[0])
+        curve_list[1].setData(data[1])
 
     def open_close_coms(self):
         global UART
@@ -119,6 +124,8 @@ class MainWindow(QMainWindow):
                 if "windows" in sys_platform:
                     UART = serial.Serial(str(self.coms[self.setting_page.comboBox.currentIndex()])[0:5],
                                          int(self.setting_page.comboBox_2.currentText()),
+                                         bytesize=serial.EIGHTBITS,
+                                         stopbits=serial.STOPBITS_ONE,
                                          timeout=0.2)
                 if "macos" in sys_platform:
                     UART = serial.Serial(str(self.coms[self.setting_page.comboBox.currentIndex()])[0:21],
@@ -126,7 +133,6 @@ class MainWindow(QMainWindow):
                                          timeout=0.2)
                 if UART.isOpen():
                     self.statusBar.showMessage("串口已成功打开!", 3000)
-                    self.plot_page_show()
                     lock = threading.Lock()
                     RX_THREAD = UART_RX_TREAD('URX1', lock)
                     RX_THREAD.setDaemon(True)
@@ -134,6 +140,8 @@ class MainWindow(QMainWindow):
                     RX_THREAD.resume()
             except:
                 self.statusBar.showMessage("串口或被占用，打开失败", 3000)
+                self.flag = 0
+                self.setting_page.pushButton_2.setText("打开串口")
         else:
             self.flag = 0
             self.setting_page.pushButton_2.setText("打开串口")
@@ -144,7 +152,9 @@ class MainWindow(QMainWindow):
         self.setting_page.comboBox.clear()
         self.coms = list(serial.tools.list_ports.comports())
         for i in range(len(self.coms)):
-            self.setting_page.comboBox.addItem(str(self.coms[i]))
+            comport = list(self.coms[i])
+            number, name = comport[0], comport[1]
+            self.setting_page.comboBox.addItem(str(number))
         self.statusBar.showMessage("串口设备已更新!", 3000)
 
     def write_bound_rate(self):
@@ -158,11 +168,132 @@ class MainWindow(QMainWindow):
 
     def setting_page_show(self):
         self.pages_manager.setCurrentIndex(0)
-        self.resize(250, 280)
+        self.resize(600, 400)
 
     def plot_page_show(self):
         self.pages_manager.setCurrentIndex(1)
         self.resize(800, 600)
+
+    def start_MCU(self):
+        global UART
+        if not self.flag_3:
+            self.action_3.setIcon(QIcon('Stop.svg'))
+            self.flag_3 = 1
+            if UART.isOpen():
+                self.plot_page_show()
+                UART.write("START\r\n".encode('utf-8'))
+        else:
+            self.action_3.setIcon(QIcon('Run.svg'))
+            self.flag_3 = 0
+            UART.write("STOP\r\n".encode('utf-8'))
+        # UART.send("START\r\n")
+
+    def sendcommand(self):
+        print("hello")
+
+    def set_channals(self):
+        global data, UART
+        global p_list, curve_list
+
+        self.str2send = 'SET'
+        self.channals_num = 0
+        p_list.clear()
+        curve_list.clear()
+        # item = self.VL.takeAt(0)
+        # while item is not None:
+        #     if item.widget():
+        #         self.VL.removeItem(item)
+        #         del item
+        #     item = self.VL.takeAt(0)
+        for i in range(self.plot_page.layout().count()):
+            self.plot_page.layout().itemAt(i).widget().deleteLater()
+
+        if self.setting_page.checkBox_1.isChecked():
+            self.str2send += '1'
+            self.channals_num += 1
+            p = Plot_Widget()
+            p.setLabel('top', 'CH1')
+            curve = p.plot(pen='y')
+            p_list.append(p)
+            curve_list.append(curve)
+        if self.setting_page.checkBox_2.isChecked():
+            self.str2send += '2'
+            self.channals_num += 1
+            p = Plot_Widget()
+            p.setLabel('top', 'CH2')
+            curve = p.plot(pen='g')
+            p_list.append(p)
+            curve_list.append(curve)
+        if self.setting_page.checkBox_3.isChecked():
+            self.str2send += '3'
+            self.channals_num += 1
+            p = Plot_Widget()
+            p.setLabel('top', 'CH3')
+            curve = p.plot(pen='b')
+            p_list.append(p)
+            curve_list.append(curve)
+        if self.setting_page.checkBox_4.isChecked():
+            self.str2send += '4'
+            self.channals_num += 1
+            p = Plot_Widget()
+            p.setLabel('top', 'CH4')
+            curve = p.plot(pen='w')
+            p_list.append(p)
+            curve_list.append(curve)
+        if self.setting_page.checkBox_5.isChecked():
+            self.str2send += '5'
+            self.channals_num += 1
+            p = Plot_Widget()
+            p.setLabel('top', 'CH5')
+            curve = p.plot(pen='p')
+            p_list.append(p)
+            curve_list.append(curve)
+        if self.setting_page.checkBox_6.isChecked():
+            self.str2send += '6'
+            self.channals_num += 1
+            p = Plot_Widget()
+            p.setLabel('top', 'CH6')
+            curve = p.plot(pen='r')
+            p_list.append(p)
+            curve_list.append(curve)
+        if self.setting_page.checkBox_7.isChecked():
+            self.str2send += '7'
+            self.channals_num += 1
+            p = Plot_Widget()
+            p.setLabel('top', 'CH7')
+            curve = p.plot(pen='g')
+            p_list.append(p)
+            curve_list.append(curve)
+        if self.setting_page.checkBox_8.isChecked():
+            self.str2send += '8'
+            self.channals_num += 1
+            p = Plot_Widget()
+            p.setLabel('top', 'CH8')
+            curve = p.plot(pen='y')
+            p_list.append(p)
+            curve_list.append(curve)
+        if self.setting_page.checkBox_9.isChecked():
+            self.str2send += '9'
+            self.channals_num += 1
+            p = Plot_Widget()
+            p.setLabel('top', 'CH9')
+            curve = p.plot(pen='b')
+            p_list.append(p)
+            curve_list.append(curve)
+        if self.setting_page.checkBox_10.isChecked():
+            self.str2send += 'A'
+            self.channals_num += 1
+            p = Plot_Widget()
+            p.setLabel('top', 'CH10')
+            curve = p.plot(pen='w')
+            p_list.append(p)
+            curve_list.append(curve)
+        for i in range(self.channals_num):
+            self.VL.addWidget(p_list[i])
+
+        self.plot_page_show()
+        self.str2send += '\r\n'
+        UART.write(self.str2send.encode('utf-8'))
 
 class UART_RX_TREAD(threading.Thread):  # 数据接收进程 部分重构
     global gui
@@ -208,31 +339,26 @@ class UART_RX_TREAD(threading.Thread):  # 数据接收进程 部分重构
         self.rx_buf = self.rx_buf.split(b'\r\n')
         self.nums = []
         for x in self.rx_buf:
-            if len(x) == 4:
-                self.nums.append(x[0:2])
-                self.nums.append(x[2:4])
+            if len(x) == 2 * int(gui.channals_num):
+                for i in range(int(gui.channals_num)):
+                    self.nums.append(x[2 * i:2 * (i + 1)])
         for i in range(len(self.nums)):
             self.nums[i] = int.from_bytes(self.nums[i], byteorder='big')/4096
         self.thread_plot.start()
 
     def plotting(self):
-        global data
-        global curve1, p1, curve2, p2, \
-            curve3, p3, curve4, p4, \
-            curve5, p5, curve6, p6, \
-            curve7, p7, curve8, p8, \
-            curve9, p9, curve10, p10
+        global data, gui
+        global p_list, curve_list
 
-        size = int(len(self.nums) / 2)
+        size = int(len(self.nums) / gui.channals_num)
         self.sum += size
         print(self.sum)
         for i in range(size):
-            data[0][i] = self.nums[2*i]
-            data[1][i] = self.nums[2*i + 1]
+            for j in range(int(gui.channals_num)):
+                data[j][i] = self.nums[gui.channals_num * i + j]
         data = np.roll(data, 32000 - size, axis=1)
-        curve1.setData(data[0])
-        curve2.setData(data[1])
-
+        for i in range(int(gui.channals_num)):
+            curve_list[i].setData(data[i])
 
 class ProcessingThread(QThread):
     signal = Signal()
